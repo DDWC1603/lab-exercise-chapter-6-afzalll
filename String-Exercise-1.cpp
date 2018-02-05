d4dbe237ca4407db50ba230d8b3c8ae79f78901e
afzalll <33884178+afzalll@users.noreply.github.com>
2018-02-05 09:51:06 +0800

    Update String-Exercise-1.cpp

--- String-Exercise-1.cpp
+++ String-Exercise-1.cpp
@@ -1,15 +1,19 @@
-//include your name and matric number here
+//Muhammad Afzal Bin Mohd Nor
+//A17DW4375
 //display the string below using the string function state below.
 
 #include <iostream>
+#include <string>
+
+using namespace std;
 
 int main()
 {
-	char engkau[10];
+	string char1 = "engkau";
 
 	//stringcopy(a,"hello");
 	
-	cout<<engkau;
+	cout<<char1;
 
 	return 0;
 }
